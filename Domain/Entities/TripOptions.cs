ed06879908434d2545af8edffcd3fc48d51541ec
hazharaziz <hazharaziz5555@gmail.com>
2022-01-20 17:58:25 +0330

    add TripOptions entity, add PaymentType enum

---
+++ Domain/Entities/TripOptions.cs
@@ -0,0 +1,22 @@
+﻿using Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class TripOptions
+    {
+        public Address SecondDestination { get; set; }
+
+        public bool RoundTrip { get; set; }
+
+        public DateTime ExtraStopDuration { get; set; }
+
+        public PaymentType PaymentType { get; set; }
+
+        public bool Hurried { get; set; }
+    }
+}
