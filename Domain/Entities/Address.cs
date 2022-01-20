a7c2f76e17c021bbd830538faa2cfe4ce924ac65
hazharaziz <hazharaziz5555@gmail.com>
2022-01-20 17:43:17 +0330

    add Address entity

---
+++ Domain/Entities/Address.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class Address
+    {
+        public double Latitude { get; set; }
+
+        public double Longitude { get; set; }
+
+        public string Description { get; set; }
+    }
+}
