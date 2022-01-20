115e371e85f18cb1428855f21d4bf8d668bce5eb
hazharaziz <hazharaziz5555@gmail.com>
2022-01-20 17:48:21 +0330

    add Driver entity

---
+++ Domain/Entities/Driver.cs
@@ -0,0 +1,16 @@
+﻿using Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class Driver
+    {
+        public CarType Car { get; set; }
+
+        public string UserId { get; set; }
+    }
+}
